b4d64b9c59c8ef9b00ea178829361d8fad7cd94a
Courtney Noonan <courtneynoonan@me.com>
2018-01-04 12:50:19 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div>
+          <h3 className="red">Courtney Noonan</h3>
+        </div>
+        <div>
+          <p>NAV STUFF</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
